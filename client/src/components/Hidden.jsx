ce9d3fdb5d709b366cbb84495fdfa4dc7c5430c7
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-03-05 22:55:38 +0200

    page navigation for hidden and pinned

--- client/src/components/Hidden.jsx
+++ client/src/components/Hidden.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import qs from "qs";
+import classNames from "classnames";
 import api from "../services/api";
 import Results from "./Results";
 
@@ -19,6 +20,15 @@ export default props => {
       .catch(err => history.push("/login"));
   }, [history, location]);
 
+  const nextPage = event => {
+    event.preventDefault();
+    const parsed = qs.parse(location.search, { ignoreQueryPrefix: true });
+    parsed.offsetId = vacancies[vacancies.length - 1].id;
+    const urlencoded = qs.stringify(parsed);
+    history.push("?" + urlencoded);
+    window.scrollTo(0, 0);
+  };
+
   const onChange = () => {
     const parsed = qs.parse(location.search, { ignoreQueryPrefix: true });
     api
@@ -34,6 +44,14 @@ export default props => {
       <div className="mt-2 shadow-xl">
         <Results vacancies={vacancies} onChange={onChange}></Results>
       </div>
+      <button
+        className={classNames("block m-auto text-brand-secondary", {
+          hidden: vacancies.length === 0
+        })}
+        onClick={nextPage}
+      >
+        next page
+      </button>
     </div>
   );
 };
