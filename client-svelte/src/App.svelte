d6c90bcc189d0620c1b41b2bc9cb111f4c93b193
Seppo Kilpelainen <seppo.kilpelainen@gmail.com>
2020-10-09 19:02:08 +0300

    split client subtrees to their own repos

--- client-svelte/src/App.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { onMount } from "svelte";
-  import { user } from "./stores/user";
-  import Router from "svelte-spa-router";
-  import { routes } from "./routes";
-</script>
-
-<Router {routes} />
